0c6f46cbad76cad977969922c35af34715c756b2
OscarF2834 <pipesegoviaalvarado@gmail.com>
2026-04-15 13:50:36 -0500

    Programacion asincrona y temporizadores

---
+++ async.js
@@ -0,0 +1,11 @@
+console.log("Este es el inicio del programa - Temporizadores");
+
+//Se ejecuta una vez, en cierta cantidad de tiempo en milisegundos
+setTimeout(()=>{
+    console.info("Esto se va a ejecutar en tiempo determinado");
+},3000);
+
+//Se ejecuta infinitamente, hasta parar en cierta cantidad de tiempo.
+setInterval(()=>{
+    console.info(new Date().toLocaleTimeString())
+},1000);
