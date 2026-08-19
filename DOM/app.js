104c678bc0054db166ae7959e9c2ec23dbf8ef7d
OscarF2834 <pipesegoviaalvarado@gmail.com>
2026-08-19 15:54:28 -0500

    Dom traversing y creacion de elementos parte 2

--- DOM/app.js
+++ DOM/app.js
@@ -184,9 +184,114 @@ console.log($cards.closest("body"));
 console.log($cards.children[3].closest("section"));
 */
 
+/*
 //Creando Elementos y Fragmentos.
 const $figure = document.createElement("figure"),
     $img = document.createElement("img"),
     $figcaption = document.createElement("figcaption"),
     $figcaptionText = document.createTextNode("Animals"),
     $cards = document.querySelector(".cards");
+    $figure2 = document.createElement("figure");
+
+$img.setAttribute("src","https://picsum.photos/400/300");
+$img.setAttribute("alt","imagen aleatoria");
+$figure.classList.add("card");
+
+$figcaption.appendChild($figcaptionText);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `
+<immg src="https://picsum.photos/400/300" alt="imagen aleatoria 3">
+<figcaption>Animals</figcaption>
+`;
+$figure.classList.add("card");
+$cards.appendChild($figure2);
+
+const estaciones = ["primavera","verano","otoño","invierno"],
+    $ul = document.createElement("ul");
+
+document.write("<h3>Estaciones del Año</h3>");
+document.body.appendChild($ul);
+
+estaciones.forEach(el  => {
+    const $li = document.createElement("li");
+    $li.textContent = el;
+    $ul.appendChild($li);
+});
+
+const continentes = ["África","América","Asia","Europa","Oceanía"],
+$ul2 = document.createElement("ul");
+
+document.write("<h3>Continentes del Mundo</h3>");
+document.body.appendChild($ul2);
+$ul2.innerHTML = "";
+continentes.forEach(el => ($ul2.innerHTML += `<li>${el}</li>`));
+
+const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+],
+$ul3 = document.createElement("ul"),
+$fragment = document.createDocumentFragment();
+
+meses.forEach(el => {
+    const $li = document.createElement("li");
+    $li.textContent = el;
+    $fragment.appendChild($li);
+});
+document.write("<h3>Meses del Año</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3);
+*/
+
+//Los templates en HTML5
+const $cards = document.querySelector(".cards"),
+    $template = document.getElementById("template-card").content,
+    $fragment = document.createDocumentFragment(),
+    cardContent = [
+        {
+            title: "Tecnología",
+            img: "https://picsum.photos/400/300"
+        },
+        {
+            title: "Animales",
+            img: "https://picsum.photos/400/300"
+        },
+        {
+            title: "Arquitectura",
+            img: "https://picsum.photos/400/300"
+        },
+        {
+            title: "Gente",
+            img: "https://picsum.photos/400/300"
+        },
+        {
+            title: "Naturaleza",
+            img: "https://picsum.photos/400/300"
+        }
+    ];
+
+cardContent.forEach(el => {
+    $template.querySelector("img").setAttribute("src",el.img);
+    $template.querySelector("img").setAttribute("alt",el.title);
+    $template.querySelector("figcaption").textContent = el.title;
+
+    let $clone = document.importNode($template,true);
+    $fragment.appendChild($clone);
+});
+
+$cards.appendChild($fragment);
+
+
